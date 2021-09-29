d981c5750af03e5a27e8fa6ffdbcced35ea0eef9
rusumuhu <83660075+rusumuhu@users.noreply.github.com>
2021-09-29 19:23:30 +0300

    старт

---
+++ rubic2/android/app/src/main/kotlin/com/example/rubic2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rubic2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rubictest2/android/app/src/main/kotlin/com/example/rubictest2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rubictest2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
